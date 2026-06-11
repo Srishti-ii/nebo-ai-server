ea9bc34447d3d239b406f3e92fb1dff6a5082612
Srishti-ii <srishtisrivastava.1291@gmail.com>
2026-06-12 01:39:52 +0530

    switch to base64 google cred

--- services/calendar.js
+++ services/calendar.js
@@ -3,9 +3,8 @@ if (!process.env.GOOGLE_CREDENTIALS_JSON) {
   throw new Error("GOOGLE_CREDENTIALS_JSON is missing");
 }
 const credentials = JSON.parse(
-  process.env.GOOGLE_CREDENTIALS_JSON
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, "base64").toString("utf-8")
 );
-
 const auth = new google.auth.GoogleAuth({
   credentials,
   scopes: [
