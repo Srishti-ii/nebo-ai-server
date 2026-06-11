5dc5010475926459f59d17a03b1e0ac2b7cf1663
Srishti-ii <srishtisrivastava.1291@gmail.com>
2026-06-12 03:58:52 +0530

    Server issue fix

--- server.js
+++ server.js
@@ -191,8 +191,8 @@ console.log("Generated slots:", formattedSlots);
 //     });
 //   }
 // });
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
