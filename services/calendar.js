aea1576e9c1e39889f6d50e296f055cbe456820f
Srishti-ii <srishtisrivastava.1291@gmail.com>
2026-06-12 02:19:56 +0530

    Variable name mismatch fixes

--- services/calendar.js
+++ services/calendar.js
@@ -1,16 +1,24 @@
 const { google } = require("googleapis");
 
-if (!process.env.GOOGLE_CREDENTIALS_BASE64) {
-  throw new Error("GOOGLE_CREDENTIALS_BASE64 is missing");
+if (!process.env.GOOGLE_CREDENTIALS_JSON) {
+  throw new Error("GOOGLE_CREDENTIALS_JSON is missing");
 }
 
-const credentials = JSON.parse(
-  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, "base64").toString("utf-8")
-);
+let credentials;
+try {
+  credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+} catch (err) {
+  throw new Error("Invalid GOOGLE_CREDENTIALS_JSON format");
+}
 
 const auth = new google.auth.GoogleAuth({
   credentials,
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
-module.exports = google.calendar({ version: "v3", auth });
+const calendar = google.calendar({
+  version: "v3",
+  auth,
+});
+
+module.exports = calendar;
