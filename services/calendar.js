ff4c6a203cf1aaa59c79af137040b6fce14d2a58
Srishti-ii <srishtisrivastava.1291@gmail.com>
2026-06-14 00:50:45 +0530

    Fixed double declarations

--- services/calendar.js
+++ services/calendar.js
@@ -21,26 +21,3 @@ google.calendar({
 module.exports =
 calendar;
 
-const {
-  client_id,
-  client_secret,
-  redirect_uris,
-} = credentials.installed;
-
-const auth =
-new google.auth.OAuth2(
-  client_id,
-  client_secret,
-  redirect_uris[0]
-);
-
-auth.setCredentials(token);
-
-const calendar =
-google.calendar({
-  version: "v3",
-  auth,
-});
-
-module.exports =
-calendar;
